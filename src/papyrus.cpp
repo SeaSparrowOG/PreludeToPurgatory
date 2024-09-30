af73ff1ff263ae7a290feb5b826adea132ea54ea
SeaSparrowOG <91731172+SeaSparrowOG@users.noreply.github.com>
2024-09-30 08:42:27 +0300

    Fixed papyrus

--- src/papyrus.cpp
+++ src/papyrus.cpp
@@ -13,16 +13,25 @@ namespace Papyrus {
     }
 
     std::vector<RE::Actor*> FetchNearbyCorpses(STATIC_ARGS) {
+        _loggerDebug("Called fetch nearby corpses");
         auto response = std::vector<RE::Actor*>();
         auto* noReanimateKeyword = DefaultObjects::ModObject<RE::BGSKeyword>("MagicNoReanimate"sv);
+        if (!noReanimateKeyword) return response;
+
         if (const auto TES = RE::TES::GetSingleton()) {
-            TES->ForEachReferenceInRange(RE::PlayerCharacter::GetSingleton(), 1500.0f, [&](RE::TESObjectREFR* a_ref) {
-                if (auto* actorRef = a_ref->As<RE::Actor>(); actorRef->IsDead() && !actorRef->HasKeyword(noReanimateKeyword)) {
-                    response.push_back(actorRef);
+            TES->ForEachReferenceInRange(RE::PlayerCharacter::GetSingleton()->AsReference(), 1500.0f, [&](RE::TESObjectREFR* a_ref) {
+                if (a_ref && a_ref->Is3DLoaded()) {
+                    if (auto* actorRef = a_ref->As<RE::Actor>()) {
+                        if (actorRef->IsDead() && !actorRef->HasKeyword(noReanimateKeyword)) {
+                            response.push_back(actorRef);
+                            _loggerDebug("  Pushing back {}", actorRef->GetName());
+                        }
+                    }
                 }
             return RE::BSContainer::ForEachResult::kContinue;
             });
         }
+        _loggerInfo("  Returning response");
         return response;
     }
 
